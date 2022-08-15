03d7776ae8be8c72b9578351725395c0ee595867
José Henrique <henriqueazevedo320@gmail.com>
2022-08-15 19:40:32 -0300

    Pequena Auteração nas Urls

--- app_usuario/urls.py
+++ app_usuario/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name = 'login'),
+    path('', views.login, name = 'login'),
     path('cadastro/', views.cadastro, name='cadastro'),
     path('index', views.index, name = 'index'),
     path('logout', views.logout, name='logout')
